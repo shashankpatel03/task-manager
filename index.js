1ebc5ada4eb80bb0be4c91a1839d7455f94eb616
Shashank Patel <shashankpatel.5a7a1@gmail.com>
2026-04-30 20:33:51 +0530

    fix port for railway

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ mongoose
   .then(() => console.log("DB connected"))
   .catch((err) => console.log(err));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
